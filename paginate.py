a9d77f666e1a3b186f6f2572c66eab2514c3c2bd
jaynamiproductions <jasonlee.yn@gmail.com>
2023-08-11 18:05:30 -0400

    tests

--- paginate.py
+++ paginate.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def paginate_tosql(url, db_name):
     conn = sqlite3.connect(db_name)
-    limit = 1000
+    limit = 1500
     offset = 0
     if url.endswith('json'):
         while True:
@@ -20,18 +20,18 @@ def paginate_tosql(url, db_name):
     else:
         while True:
             base = url
-            api = base + '?limit='+ str(limit) +'&offset='+str(offset)
+            api = base + '?size='+ str(limit) +'&offset='+str(offset)
             resp = requests.get(api).json()
             df = pd.DataFrame(resp)
             df = df.applymap(str)
             df.to_sql('table_name',conn,if_exists='append',index=False)
             if df.shape[0] != limit:
                 break
-            offset += 1000     
+            offset += 1500    
 
 # TEST URLs 
-# (open-source Socrata dataset from CDC)
+# (open-source Socrata dataset from CDC, 240k rows)
 # paginate_tosql('https://data.cdc.gov/resource/bugr-bbfr.json','database.db')
 
-# (open-source dataset from CMS)
-# paginate_tosql('https://data.cms.gov/data-api/v1/dataset/94d00f36-73ce-4520-9b3f-83cd3cded25c/data','cms.db')
+# (open-source dataset from CMS, 1.8m rows)
+# paginate_tosql('https://data.cms.gov/data-api/v1/dataset/c99b5865-1119-4436-bb80-c5af2773ea1f/data','new.db')
